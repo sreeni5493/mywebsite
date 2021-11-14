bfa439771eaffd1ac09a18625d01bb04172bb9aa
sreeni5493 <sreenivas.nm5493@gmail.com>
2021-11-15 02:14:37 +0530

    UI phase 1 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import TopBar from "./topbar/TopBar";
-import Home from "./pages/home/Home"
+import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Write from "./pages/write/Write";
+import Single from "./pages/single/Single"
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
-    <TopBar />
-    <Home />
-    </>
+    
+    <BrowserRouter>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="register/*" element={user? <Home /> : <Register />} />
+        <Route path="login/*" element={user? <Home /> : <Login />} />
+        <Route path="settings/*" element={user? <Settings /> : <Login />} />
+        <Route path="write/*" element={user? <Write /> : <Login />} />
+        <Route path="post/:postId" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,58 @@
+import "./topbar.css";
+// import logo from "../assets/logo.jpg";
+import userIcon from "../assets/user_icon.jpg";
+import { Link } from "react-router-dom";
+
+export default function TopBar() {
+  const user = false;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        {/* <i className="topIcon fab fa-linkedin"></i>
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i> */}
+        {/* <img className="logo" src={logo} alt="My Logo"></img> */}
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/about">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">{user && "LOGOUT"}</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <img className="topImage" src={userIcon} alt=""></img>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
